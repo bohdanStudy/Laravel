d56463cd83e483174c6e9a771ea519047c22ff5f
bogda <bohdan.mychka@oa.edu.ua>
2026-06-01 10:23:16 +0000

    Lab 9

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Blog\Admin\CategoryController;
-use App\Http\Controllers\Api\Blog\PostController;
+use App\Http\Controllers\Api\Blog\Admin\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
