7e212fabb5b488d565a2a9d062d6139404c24c1e
bogda <bohdan.mychka@oa.edu.ua>
2026-06-01 10:33:38 +0000

    Lab 10

--- app/Http/Controllers/Api/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Api/Blog/Admin/CategoryController.php
@@ -37,10 +37,6 @@ class CategoryController extends BaseController
     {
         $data = $request->input(); // отримаємо масив даних, які надійшли з форми
 
-        if (empty($data['slug'])) { // якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); // генеруємо псевдонім
-        }
-
         $item = (new BlogCategory())->create($data); // створюємо об'єкт і додаємо в БД
 
         if ($item) {
@@ -75,9 +71,6 @@ class CategoryController extends BaseController
         }
 
         $data = $request->all(); //отримаємо масив даних, які надійшли з форми
-        if (empty($data['slug'])) { //якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
-        }
 
         $result = $item->update($data);  //оновлюємо дані об'єкта і зберігаємо в БД
 
